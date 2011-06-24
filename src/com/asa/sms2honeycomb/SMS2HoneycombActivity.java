6a785eeca101fc50734f72461c746864218261fc
Aaron Rietschlin <nemisis.82@gmail.com>
2011-06-24 18:49:45 -0400

    Added Source files

---
+++ src/com/asa/sms2honeycomb/SMS2HoneycombActivity.java
@@ -0,0 +1,13 @@
+package com.asa.sms2honeycomb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SMS2HoneycombActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
