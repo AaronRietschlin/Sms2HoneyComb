cb9f3ed0213430ad8f496acd5dc0946c9a4fa178
Aaron Rietschlin <nemisis.82@gmail.com>
2011-08-12 18:50:41 -0400

    Added registration code

--- gen/com/asa/sms2honeycomb/R.java
+++ gen/com/asa/sms2honeycomb/R.java
@@ -10,35 +10,38 @@ package com.asa.sms2honeycomb;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int red=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int blank_space=0x7f05000c;
-        public static final int fragment_container=0x7f050000;
-        public static final int launcher_login_btn_phone=0x7f050006;
-        public static final int launcher_register_btn_phone=0x7f050007;
-        public static final int linearLayout1=0x7f050011;
-        public static final int linearLayout2=0x7f050015;
-        public static final int login_cancel_btn_phone=0x7f05000e;
-        public static final int login_login_btn_phone=0x7f05000d;
-        public static final int login_password_field_phone=0x7f05000b;
-        public static final int login_password_title_phone=0x7f05000a;
-        public static final int login_username_field_phone=0x7f050009;
-        public static final int login_username_title_phone=0x7f050008;
-        public static final int message_input=0x7f050002;
-        public static final int message_view_fragment=0x7f050001;
-        public static final int messages_layout=0x7f050004;
-        public static final int register_email_phone=0x7f050010;
-        public static final int register_email_text_phone=0x7f05000f;
-        public static final int register_invalid_email_phone=0x7f050012;
-        public static final int register_invalid_username_phone=0x7f050016;
-        public static final int register_password_phone=0x7f050018;
-        public static final int register_password_text_phone=0x7f050017;
-        public static final int register_username_phone=0x7f050014;
-        public static final int register_username_text_phone=0x7f050013;
-        public static final int send_message_button=0x7f050003;
-        public static final int textView1=0x7f050005;
+        public static final int blank_space=0x7f07000c;
+        public static final int fragment_container=0x7f070000;
+        public static final int launcher_login_btn_phone=0x7f070006;
+        public static final int launcher_register_btn_phone=0x7f070007;
+        public static final int linearLayout1=0x7f070011;
+        public static final int linearLayout2=0x7f070015;
+        public static final int login_cancel_btn_phone=0x7f07000e;
+        public static final int login_login_btn_phone=0x7f07000d;
+        public static final int login_password_field_phone=0x7f07000b;
+        public static final int login_password_title_phone=0x7f07000a;
+        public static final int login_username_field_phone=0x7f070009;
+        public static final int login_username_title_phone=0x7f070008;
+        public static final int message_input=0x7f070002;
+        public static final int message_view_fragment=0x7f070001;
+        public static final int messages_layout=0x7f070004;
+        public static final int register_email_phone=0x7f070010;
+        public static final int register_email_text_phone=0x7f07000f;
+        public static final int register_invalid_email_phone=0x7f070012;
+        public static final int register_invalid_username_phone=0x7f070016;
+        public static final int register_password_phone=0x7f070018;
+        public static final int register_password_text_phone=0x7f070017;
+        public static final int register_username_phone=0x7f070014;
+        public static final int register_username_text_phone=0x7f070013;
+        public static final int send_message_button=0x7f070003;
+        public static final int textView1=0x7f070005;
     }
     public static final class layout {
         public static final int activity_tablet_main=0x7f030000;
@@ -50,15 +53,23 @@ public final class R {
         public static final int register_phone=0x7f030006;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int launcher_login_btn_text=0x7f040004;
-        public static final int launcher_register_btn_text=0x7f040003;
-        public static final int login_cancel_btn=0x7f040005;
-        public static final int login_password_hint=0x7f040009;
-        public static final int login_password_text=0x7f040007;
-        public static final int login_username_hint=0x7f040008;
-        public static final int login_username_text=0x7f040006;
-        public static final int send_msg_btn_text=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int launcher_login_btn_text=0x7f050004;
+        public static final int launcher_register_btn_text=0x7f050003;
+        public static final int login_cancel_btn=0x7f050005;
+        public static final int login_password_hint=0x7f050009;
+        public static final int login_password_text=0x7f050007;
+        public static final int login_username_hint=0x7f050008;
+        public static final int login_username_text=0x7f050006;
+        public static final int register_duplicate_email=0x7f05000e;
+        public static final int register_enter_email=0x7f05000b;
+        public static final int register_enter_password=0x7f05000c;
+        public static final int register_enter_username=0x7f05000a;
+        public static final int register_username_taken=0x7f05000d;
+        public static final int send_msg_btn_text=0x7f050002;
+    }
+    public static final class style {
+        public static final int InvalidRegistration=0x7f060000;
     }
 }
--- src/phone/RegisterActivity.java
+++ src/phone/RegisterActivity.java
@@ -1,16 +1,113 @@
 package phone;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.asa.sms2honeycomb.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
-public class RegisterActivity extends Activity{
+public class RegisterActivity extends Activity {
+
+	private Button registerButton;
+	private Button cancelButton;
+	private EditText emailField;
+	private EditText usernameField;
+	private EditText passwordField;
+	private TextView invalidEmail;
+	private TextView invalidUsername;
+	private TextView invalidPassword;
+
+	private boolean registrationSuccess;
+	private int errorReason = 0;
+	private final int EMPTY_USERNAME = 1;
+	private final int EMPTY_PASSWORD = 2;
+	private final int EMPTY_EMAIL = 3;
+	private final String TAG = "RegisterActivity";
+	private boolean isInTable = false;
+	private boolean DEBUG = true;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_activity_phone);
+
+		registerButton = (Button) findViewById(R.id.login_login_btn_phone);
+		cancelButton = (Button) findViewById(R.id.login_cancel_btn_phone);
+		emailField = (EditText) findViewById(R.id.register_email_phone);
+		usernameField = (EditText) findViewById(R.id.register_username_phone);
+		passwordField = (EditText) findViewById(R.id.register_password_phone);
+
+		// TODO: For Testing purposes
+		emailField.setText("email@email.com");
+		usernameField.setText("Nemisis");
+		passwordField.setText("12345");
+
+		registerButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				registrationSuccess = registerUser();
+				finish();
+			}
+		});
+		cancelButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				finish();
+			}
+		});
+
 	}
 	
+	private boolean registerUser() {
+		final String emailText = emailField.getText().toString().trim();
+		String usernameText = usernameField.getText().toString().trim();
+		String passwordText = passwordField.getText().toString().trim();
+		ParseObject userTable = new ParseObject("UserTable");
+
+		
+		ParseQuery query = new ParseQuery("UserTable");
+		query.findInBackground(new FindCallback() {
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e == null) {
+					for(ParseObject object : objects){
+						if(object.getString("email").compareTo(emailText) == 0){
+							isInTable = true;
+							break;
+						}
+					}
+					Log.e(TAG, "IsInTable = " + String.valueOf(isInTable));
+				} else {
+					Log.e(TAG, "Retrieve from database failed.");
+				}
+			}
+		});
+
+		userTable.put("email", emailText);
+		userTable.put("username", usernameText);
+		userTable.put("password", passwordText); // TODO: Encrypt passwords!
+		userTable.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e != null) {
+					Log.e(TAG, "Save failed.");
+					e.printStackTrace();
+				} else {
+					if (DEBUG)
+						Log.e(TAG, "Save success.");
+				}
+
+			}
+		});
+		return true;
+	}
 }
