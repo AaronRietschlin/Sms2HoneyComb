ae0645d1a6e67458b02734b3be83f5b68274d419
Aaron Rietschlin <nemisis.82@gmail.com>
2011-08-17 01:22:33 -0400

    Added check to see if user is logged in and directs to correct activity.

--- src/com/asa/sms2honeycomb/phone/LauncherActivity.java
+++ src/com/asa/sms2honeycomb/phone/LauncherActivity.java
@@ -8,6 +8,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.asa.sms2honeycomb.R;
+import com.parse.ParseUser;
 
 public class LauncherActivity extends Activity{
 	Button registerButton;
@@ -17,22 +18,31 @@ public class LauncherActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.launcher_phone);
 		
-		registerButton = (Button) findViewById(R.id.launcher_register_btn_phone);
-		loginButton = (Button) findViewById(R.id.launcher_login_btn_phone);
-		
-		registerButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View view) {
-				mIntent = new Intent(LauncherActivity.this, RegisterActivity.class);
-				startActivity(mIntent);
-			}
-		});
-		loginButton.setOnClickListener(new OnClickListener(){
-			public void onClick(View view) {
-				mIntent = new Intent(LauncherActivity.this, LoginActivity.class);
-				startActivity(mIntent);
-			}
-		});
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if(currentUser == null){
+			//User is not logged in.
+			setContentView(R.layout.launcher_phone);
+			
+			registerButton = (Button) findViewById(R.id.launcher_register_btn_phone);
+			loginButton = (Button) findViewById(R.id.launcher_login_btn_phone);
+			
+			registerButton.setOnClickListener(new OnClickListener(){
+				public void onClick(View view) {
+					mIntent = new Intent(LauncherActivity.this, RegisterActivity.class);
+					startActivity(mIntent);
+				}
+			});
+			loginButton.setOnClickListener(new OnClickListener(){
+				public void onClick(View view) {
+					mIntent = new Intent(LauncherActivity.this, LoginActivity.class);
+					startActivity(mIntent);
+				}
+			});
+		}else{
+			//User is logged in.
+			mIntent = new Intent(LauncherActivity.this, MainPhoneActivity.class);
+			startActivity(mIntent);
+		}
 	}
 }
