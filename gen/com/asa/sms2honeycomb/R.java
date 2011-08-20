4c642770063aabf427144ef81b76b0d20ae75c85
Parker Williams <ameriod@gmail.com>
2011-08-20 17:03:44 -0400

    Signed-off-by: Parker Williams <ameriod@gmail.com>

--- gen/com/asa/sms2honeycomb/R.java
+++ gen/com/asa/sms2honeycomb/R.java
@@ -64,6 +64,7 @@ public final class R {
         public static final int login_phone=0x7f030003;
         public static final int main_phone=0x7f030004;
         public static final int register_activity_phone=0x7f030005;
+        public static final int register_phone=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
