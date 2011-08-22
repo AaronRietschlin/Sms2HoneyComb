c4c233c69438b7ddc781b4e8356752cc8f9977a8
Aaron Rietschlin <nemisis.82@gmail.com>
2011-08-21 22:17:44 -0400

    Categorized Strings.

---
+++ src/com/asa/sms2honeycomb/tablet/LauncherActivity.java
@@ -0,0 +1,13 @@
+package com.asa.sms2honeycomb.tablet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+}
