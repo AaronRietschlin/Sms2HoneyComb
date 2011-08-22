ca3f3e77c4580d9e59fd64aa56554eb34d599a05
Aaron Rietschlin <nemisis.82@gmail.com>
2011-08-21 22:35:11 -0400

    Began implementing login for Tablets

--- src/com/asa/sms2honeycomb/tablet/LauncherActivity.java
+++ src/com/asa/sms2honeycomb/tablet/LauncherActivity.java
@@ -1,5 +1,7 @@
 package com.asa.sms2honeycomb.tablet;
 
+import com.asa.sms2honeycomb.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -7,7 +9,7 @@ public class LauncherActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.launcher_activity_tablet);
 		
 	}
 }
