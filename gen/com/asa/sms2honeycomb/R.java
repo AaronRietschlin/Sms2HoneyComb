db1e5b0e8980a8aa4b4491b9afe37416286f3325
Parker Williams <ameriod@gmail.com>
2011-08-31 17:56:55 -0400

    Signed-off-by: Parker Williams <ameriod@gmail.com>

--- gen/com/asa/sms2honeycomb/R.java
+++ gen/com/asa/sms2honeycomb/R.java
@@ -18,7 +18,6 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-<<<<<<< HEAD
         public static final int blank_space=0x7f070018;
         public static final int contacts_fragment=0x7f070001;
         public static final int conversation_new_btn=0x7f070004;
@@ -75,59 +74,6 @@ public final class R {
         public static final int register_activity_phone=0x7f03000a;
         public static final int register_activity_tablet=0x7f03000b;
         public static final int register_phone=0x7f03000c;
-=======
-        public static final int blank_space=0x7f070014;
-        public static final int contacts_fragment=0x7f070001;
-        public static final int forgot_password_button_tablet=0x7f07000d;
-        public static final int fragment_container=0x7f070000;
-        public static final int launcher_login_btn_phone=0x7f070007;
-        public static final int launcher_register_btn_phone=0x7f070008;
-        public static final int login_button_tablet=0x7f07000b;
-        public static final int login_create_account_btn_phone=0x7f070012;
-        public static final int login_create_account_btn_tab=0x7f07000c;
-        public static final int login_forgot_password_button_phone=0x7f070013;
-        public static final int login_login_btn_phone=0x7f070015;
-        public static final int login_password_field_phone=0x7f070011;
-        public static final int login_password_field_tablet=0x7f070009;
-        public static final int login_password_title_phone=0x7f070010;
-        public static final int login_username_field_phone=0x7f07000f;
-        public static final int login_username_field_tablet=0x7f07000a;
-        public static final int login_username_title_phone=0x7f07000e;
-        public static final int main_message_felid=0x7f070019;
-        public static final int main_send_btn=0x7f07001a;
-        public static final int message_fragment=0x7f070002;
-        public static final int message_input=0x7f070003;
-        public static final int messages_layout=0x7f070005;
-        public static final int phone_text_button=0x7f070018;
-        public static final int phone_to_field=0x7f070017;
-        public static final int phone_to_layout=0x7f070016;
-        public static final int register_btn_phone=0x7f070021;
-        public static final int register_button_tablet=0x7f070025;
-        public static final int register_email_field_tab=0x7f070022;
-        public static final int register_email_phone=0x7f07001c;
-        public static final int register_email_text_phone=0x7f07001b;
-        public static final int register_password_field_tab=0x7f070024;
-        public static final int register_password_phone=0x7f070020;
-        public static final int register_password_text_phone=0x7f07001f;
-        public static final int register_username_field_tab=0x7f070023;
-        public static final int register_username_phone=0x7f07001e;
-        public static final int register_username_text_phone=0x7f07001d;
-        public static final int send_message_button=0x7f070004;
-        public static final int textView1=0x7f070006;
-    }
-    public static final class layout {
-        public static final int activity_tablet_main=0x7f030000;
-        public static final int fragment_message_view=0x7f030001;
-        public static final int launcher_phone=0x7f030002;
-        public static final int list_item=0x7f030003;
-        public static final int login_activity_tablet=0x7f030004;
-        public static final int login_phone=0x7f030005;
-        public static final int main_phone=0x7f030006;
-        public static final int message_list=0x7f030007;
-        public static final int register_activity_phone=0x7f030008;
-        public static final int register_activity_tablet=0x7f030009;
-        public static final int register_phone=0x7f03000a;
->>>>>>> origin/master
     }
     public static final class string {
         public static final int app_name=0x7f050001;
